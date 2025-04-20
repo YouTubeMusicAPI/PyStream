1395f208e8c40513b79719f4be715da629690998
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-20 22:02:54 +0530

    Update Audio.py

--- PyStream/Audio.py
+++ PyStream/Audio.py
@@ -6,7 +6,6 @@ class AudioHandler:
     @staticmethod
     def convert_to_mp3(input_file: str, output_file: str) -> None:
         if not os.path.exists(input_file):
-            print(f"Error: Input file '{input_file}' does not exist.")
             return
 
         try:
@@ -17,21 +16,17 @@ class AudioHandler:
                 output_file
             ]
             subprocess.run(cmd, check=True)
-            print(f"Conversion successful: {output_file}")
 
-            # After conversion, check if the file exists and remove it
             if os.path.exists(input_file):
                 os.remove(input_file)
-                print(f"Deleted original file: {input_file}")
         except subprocess.CalledProcessError as e:
-            print(f"Error during conversion: {e}")
+            pass
         except Exception as e:
-            print(f"An unexpected error occurred during conversion: {e}")
+            pass
 
     @staticmethod
     def stream_audio(input_file: str, output_file: str) -> None:
         if not os.path.exists(input_file):
-            print(f"Error: Input file '{input_file}' does not exist.")
             return
 
         try:
@@ -42,16 +37,10 @@ class AudioHandler:
                 output_file
             ]
             subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            print(f"Streaming started for {output_file}")
         except Exception as e:
-            print(f"Error during streaming: {e}")
+            pass
 
     @staticmethod
     def create_temp_file() -> str:
-        try:
-            temp_file = NamedTemporaryFile(delete=False, suffix='.mp3')
-            temp_file.close()  # Ensure the file is created and closed before returning
-            return temp_file.name
-        except Exception as e:
-            print(f"Error creating temporary file: {e}")
-            return ""
+        temp_file = NamedTemporaryFile(delete=False, suffix='.mp3')
+        return temp_file.name
