50a3283f208814a19f52a48560ab894c0ed1150d
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-20 20:38:58 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,8 +5,15 @@ from YouTubeMusic.YtSearch import Search
 from yt_dlp import YoutubeDL
 import asyncio
 
-vc = PyStream(Client)
+API_ID = 6067591
+API_HASH = "94e17044c2393f43fda31d3afe77b26b"
+SESSION = "BQBclYcAfwPhsOaYEN9rZiJTeqV1e-mW90J3pxU5lU-HRDBDir4n236Uy6xowZLnSJ83DDyV-7m8NommEpFKXVZMwRR41bXxvE8JzhIcLIJnCP5yObgE3yRkljsE36qEsdVYTgggdMSHrhoFWZG5YuOIJ0hi1HpqzOJhocARqoVbys1-CNSjTAEXdNB3knhatAqkHVnHfWcgvtshc3iiru3Gjpl9lXaPnLL5p5GP11dL8vRS4Dob-8nZW2vEkXqsD4-Ce6BAD8m4RIqTsomtrQCgaH4ugYfpFuKVr_oz04hUTjB4MzXK-Wr_Fz5Lk42PnrE3wWEwhsfgOVu8AM02YlKLV77MegAAAAHKUdR6AA"
+
+app = Client("MusicBot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
+
+vc = PyStream(app)
 queue = AudioQueue()
+
 
 YDL_OPTS = {
     "format": "bestaudio[ext=m4a]/bestaudio/best",
