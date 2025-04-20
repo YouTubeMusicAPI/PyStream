167cf624a552b388ac8e03a8480b9b11cab3fd5e
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-20 16:07:37 +0530

    Update Utils.py

--- PyStream/Utils.py
+++ PyStream/Utils.py
@@ -1,28 +1,40 @@
 import yt_dlp
 import re
 import os
+from YouTubeMusic.YtSearch import Search
 from .Audio import AudioHandler
 
-def download_audio(url: str, output_format='mp3') -> str:
+async def download_audio(query: str, output_format='mp3') -> str:
+    search_results = await Search(query, limit=1)
+    if not search_results:
+        raise Exception(f"Song '{query}' not found.")
+    
+    url = search_results[0]["url"]
+
     ydl_opts = {
         'format': 'bestaudio/best',
         'extractaudio': True,
         'audioquality': 1,
-        'outtmpl': f'./downloads/%(id)s.%(ext)s',
+        'outtmpl': './downloads/%(id)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegAudio',
             'preferredcodec': output_format,
             'preferredquality': '192',
         }],
+        'cookiefile': cookies/cookies.txt,
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info_dict)
-        output_path = filename.replace(".webm", f".{output_format}")
-        AudioHandler.convert_to_mp3(filename, output_path)
-        os.remove(filename)
-        return output_path
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            data = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(data)
+            output_path = filename.replace(".webm", f".{output_format}")
+            AudioHandler.convert_to_mp3(filename, output_path)
+            os.remove(filename)
+    except Exception as e:
+        raise Exception(f"Error downloading the song: {e}")
+
+    return output_path
 
 def validate_url(url: str) -> bool:
     youtube_pattern = r'(https?://(?:www\.)?youtube\.com/watch\?v=[\w-]+)'
