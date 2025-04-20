ea9e929edb0ba5d2eaa6be5cc7fb66ae4ea6d4c6
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-20 15:21:18 +0530

    Update __init__.py

--- PyStream/__init__.py
+++ PyStream/__init__.py
@@ -4,3 +4,19 @@ from .Queue import AudioQueue
 from .Types import Track, QueueItem
 from .Exceptions import StreamException, VCJoinError, InvalidURL, QueueEmptyError
 from .Utils import download_audio, validate_url, get_video_duration
+
+
+__all__ = [
+    "PyStream",
+    "AudioHandler",
+    "AudioQueue",
+    "Track",
+    "QueueItem",
+    "StreamException",
+    "VCJoinError",
+    "InvalidURL",
+    "QueueEmptyError",
+    "download_audio",
+    "validate_url",
+    "get_video_duration"
+]
