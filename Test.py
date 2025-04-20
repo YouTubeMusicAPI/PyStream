94ae18e040746d0f824bc0a76cea3d09c423b67c
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-20 22:58:45 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ import asyncio
 
 API_ID = 6067591
 API_HASH = "94e17044c2393f43fda31d3afe77b26b"
-SESSION = "your_session_here"
+SESSION = "BQBclYcAfwPhsOaYEN9rZiJTeqV1e-mW90J3pxU5lU-HRDBDir4n236Uy6xowZLnSJ83DDyV-7m8NommEpFKXVZMwRR41bXxvE8JzhIcLIJnCP5yObgE3yRkljsE36qEsdVYTgggdMSHrhoFWZG5YuOIJ0hi1HpqzOJhocARqoVbys1-CNSjTAEXdNB3knhatAqkHVnHfWcgvtshc3iiru3Gjpl9lXaPnLL5p5GP11dL8vRS4Dob-8nZW2vEkXqsD4-Ce6BAD8m4RIqTsomtrQCgaH4ugYfpFuKVr_oz04hUTjB4MzXK-Wr_Fz5Lk42PnrE3wWEwhsfgOVu8AM02YlKLV77MegAAAAHKUdR6AA"
 
 app = Client("MusicBot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
 pystream = PyStream(app)
