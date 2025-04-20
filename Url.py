af764e7a4a1ca67d3e709303ffb0801e0c1dddb0
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-20 21:20:27 +0530

    Update Url.py

--- Url.py
+++ Url.py
@@ -7,7 +7,6 @@ def download_audio(url: str, output_format='mp3') -> str:
         'extractaudio': True,
         'audioquality': 1,
         'outtmpl': f'./downloads/%(id)s.%(ext)s',
-        'cookiefile': 'cookies/cookies.txt',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': output_format,
@@ -20,17 +19,22 @@ def download_audio(url: str, output_format='mp3') -> str:
             info_dict = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info_dict)
             output_path = filename.replace(".webm", f".{output_format}")
-            os.remove(filename)
+            
+            # Check if the .webm file exists before trying to delete it
+            if os.path.exists(filename):
+                os.remove(filename)
+                
             return output_path
 
     except Exception as e:
         print(f"Error downloading or converting audio: {e}")
         return None
 
-url = "https://youtu.be/bVNJVB10C6w?si=kqUhtB9yZ5Da83lk"  
+url = "https://youtu.be/bVNJVB10C6w?si=kqUhtB9yZ5Da83lk"  # Replace with the actual URL you want
 
 downloaded_file = download_audio(url)
 if downloaded_file:
     print(f"Audio downloaded successfully: {downloaded_file}")
 else:
     print("Failed to download audio.")
+    
